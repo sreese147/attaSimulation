7ce0c0687298824bde308a89b8a76383aa3b4c57
Sabrina <33787198+sreese147@users.noreply.github.com>
2023-03-15 13:27:30 -0400

    Update scheduledFlight.py

--- scheduledFlight.py
+++ scheduledFlight.py
@@ -1,6 +1,6 @@
 """Sabrina Reese
 Cmdr Schenk
-Feb 9 2023
+march 15 2023
 progress check 1
 scheduled flight
 period 5 cs50 ap"""
@@ -14,14 +14,14 @@ def main():
 
     fake = Faker()
     fake.add_provider(AirTravelProvider)
-
+    
 
     status = ["Awaiting", "Boarding", "Departing", "InFlight -- OnTime", "InFlight -- Late", "Landed - On Time", "Landed - Late", "Cancelled", "Delayed", "Unknown"]
-    values = [ str(random.randint(0,9999)), str(random.randint(0,9999)), random.choice(status), fake.airport_icao(), fake.airport_icao(), str(random.randint(0,9999)), str(fake.past_datetime()), str(fake.past_datetime()), 'Foreign key passengers', 'foreign key crew', 'foreign key aircraft ' ]
-
-    query = f"INSERT INTO scheduledFlight () VALUES ()" + ','.join(values)
-
-    print(query)
+    for i in range(1,5):
+        values = [ str(random.randint(0,9999)), str(random.randint(0,9999)), random.choice(status), fake.airport_icao(), fake.airport_icao(), str(random.randint(0,9999)), str(fake.past_datetime()), str(fake.past_datetime()), 'Foreign key passengers', 'foreign key crew', 'foreign key aircraft ' ]
+        query = f"INSERT INTO scheduledFlight () VALUES ()" + ','.join(values)
+        print(query)
+        time.sleep(3)
 
 if(__name__=="__main__"):
     main()
