cfd6d7de86075e28bd4e5cc84fed8c7f0f94d11c
Sabrina <33787198+sreese147@users.noreply.github.com>
2023-03-21 13:20:20 -0400

    Update scheduledFlight.py

--- scheduledFlight.py
+++ scheduledFlight.py
@@ -1,7 +1,6 @@
-"""Sabrina Reese
+"""Sabrina Reese + Rose Coluzzi
 Cmdr Schenk
-march 15 2023
-progress check 1
+sprint 2
 scheduled flight
 period 5 cs50 ap"""
 import time
@@ -15,22 +14,23 @@ import mysql.connector
 
 def main():
 
-    connection = sqlite3.connect('s.db')
-    cursor = connection.cursor()
-
+    #Sabrina Reese
+    #Set the faker library to variable fake
+    #and get the air travel section of the faker library
     fake = Faker()
     fake.add_provider(AirTravelProvider)
-    
-
     status= ["Awaiting", "Boarding", "Departing", "InFlight - OnTime", "InFlight - Late", "Landed - On Time", "Landed - Late", "Cancelled", "Delayed", "Unknown"]
     
     for i in range(1,5):
+        #Sabrina Reese
+        #insert sql rec
         query = """INSERT INTO scheduled_flights (ein, status, departureField, arrivalField, distance, scheduled_departure, scheduled_arrival, passengers, crew,aircraft) VALUES ({0}, '{1}', '{2}','{3}',{4}, '{5}', '{6}','Foreign key passengers', 'fk crew', 'fk aircraft')""".format((random.randint(0,9999)),str(random.choice(status)), fake.airport_icao(),fake.airport_icao(),  random.randint(0,9999),str(fake.past_datetime()), str(fake.past_datetime()))
         cursor.execute(query)
         connection.commit()
         cursorFetch = cursor.execute("SELECT * FROM scheduled_flights ORDER BY id DESC;")
-        lastRec = cursorFetch.fetchone()
-        print(lastRec)
+        rec = cursorFetch.fetchone()
+        print(rec)
+        #repeat every 3 min (3 sec)
         time.sleep(3)
 
 if(__name__=="__main__"):
