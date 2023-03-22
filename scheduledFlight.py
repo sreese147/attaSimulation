bdb13d9ae6ea7657025307ac1c72f0308a42e428
Sabrina <33787198+sreese147@users.noreply.github.com>
2023-03-22 13:07:55 -0400

    Update scheduledFlight.py

--- scheduledFlight.py
+++ scheduledFlight.py
@@ -28,6 +28,8 @@ def main():
     fake = Faker()
     fake.add_provider(AirTravelProvider)
     
+    
+    
     status= ["Awaiting", "Boarding", "Departing", "InFlight - OnTime", "InFlight - Late", "Landed - On Time", "Landed - Late", "Cancelled", "Delayed", "Unknown"]
     
     #Rose Coluzzi 
@@ -35,17 +37,25 @@ def main():
     crewX = crewDB.read()
     aircraftX = aircraftDB.read()
     
-    for i in range(1,5):
+    #to avoid forever loops, the program needs to be restarted every "day"
+    for i in range(1,480):
         #Sabrina Reese
+        time.sleep(1)
+        #make sure that the departure time is before the arrival time
+        #if it is not, it reassigns a new departure time
+        dept = fake.past_datetime()
+        arri = fake.past_datetime()
+        while dept > arri:
+            dept = fake.past_datetime()
         #insert sql rec
-        query = """INSERT INTO scheduled_flights (ein, status, departureField, arrivalField, distance, scheduled_departure, scheduled_arrival, passengers, crew,aircraft) VALUES ({0}, '{1}', '{2}','{3}',{4}, '{5}', '{6}','{7}', '{8}', '{9}')""".format((random.randint(0,9999)),str(random.choice(status)), fake.airport_icao(),fake.airport_icao(),  random.randint(0,9999),str(fake.past_datetime()), str(fake.past_datetime()), str(passengerX[0][3]), str(crewX[0][3]), str(aircraftX[0][3]))
+        query = """INSERT INTO scheduled_flights (ein, status, departureField, arrivalField, distance, scheduled_departure, scheduled_arrival, passengers, crew,aircraft) VALUES ({0}, '{1}', '{2}','{3}',{4}, '{5}', '{6}','{7}', '{8}', '{9}')""".format((random.randint(0,9999)),str(random.choice(status)), fake.airport_icao(),fake.airport_icao(),  random.randint(0,9999),str(dept), str(arri), str(passengerX[0][3]), str(crewX[0][3]), str(aircraftX[0][3]))
         cursor.execute(query)
         connection.commit()
         cursorFetch = cursor.execute("SELECT * FROM scheduled_flights ORDER BY id DESC;")
         rec = cursorFetch.fetchone()
         print(rec)
         #repeat every 3 min (3 sec)
-        time.sleep(3)
+        time.sleep(2)
 
 if(__name__=="__main__"):
     main()
