b49eb83cc3150c96a2c2aac5e49c2de9aedc2787
VivekJohn02 <96063037+VivekJohn02@users.noreply.github.com>
2023-03-23 11:00:55 -0400

    Update RngAirportsql.py

--- RngAirportsql.py
+++ RngAirportsql.py
@@ -10,6 +10,6 @@ def breakConn(self):
         self.connection.close()
         self.cursor.close()
 def __init__(self):
-    self.cursor.execute(f"INSERT INTO passenger (icao_code,iata_code,city,country,lat_deg,lat_min,lat_sec,lat_dir,lon_deg,lon_min,lon_sec,lon_dir,altitude,lat_decimal,lon_decimal) VALUES ('{random.choice(results)}','{random.choice(results2)}','{random.choice(results3)}','{random.choice(results4)}', '{random.choice(results5)}','{random.choice(results6)}', '{random.choice(results7)}', '{random.choice(results8)}')")
+    self.cursor.execute(f"INSERT INTO airports (icao_code,iata_code,city,country,lat_deg,lat_min,lat_sec,lat_dir,lon_deg,lon_min,lon_sec,lon_dir,altitude,lat_decimal,lon_decimal) VALUES ('{random.choice(results)}','{random.choice(results2)}','{random.choice(results3)}','{random.choice(results4)}', '{random.choice(results5)}','{random.choice(results6)}', '{random.choice(results7)}', '{random.choice(results8)}')")
 
     time.sleep(5)
